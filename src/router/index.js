cfcadd576f074d7d0ab2ad88a107ba79a727992f
heloisabett <heloisabettramosifc@gmail.com>
2023-06-19 16:03:36 -0300

    parte 9

---
+++ src/router/index.js
@@ -0,0 +1,28 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import CategoriaView from '../views/CategoriaView.vue';
+import AcessorioView from '../views/AcessorioView.vue';
+
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/categorias',
+      name: 'categorias',
+      component: CategoriaView,
+    },
+    {
+      path: '/acessorios',
+      name: 'acessorios',
+      component: AcessorioView,
+    },
+  ],
+});
+
+export default router;
