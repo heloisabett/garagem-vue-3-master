32fd692d23c1129f8d96650ef4f5c9620164d197
heloisabett <heloisabettramosifc@gmail.com>
2023-06-22 15:31:14 -0300

    parte 9.2

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import CategoriaView from '../views/CategoriaView.vue';
 import AcessorioView from '../views/AcessorioView.vue';
-
+import MarcaView from "../views/MarcaView.vue";
+import CorView from "../views/CorView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,6 +22,16 @@ const router = createRouter({
       path: '/acessorios',
       name: 'acessorios',
       component: AcessorioView,
+    },
+    {
+      path: "/marcas",
+      name: "marcas",
+      component: MarcaView,
+    },
+    {
+      path: "/cores",
+      name: "cores",
+      component: CorView,
     },
   ],
 });
